03d326d1266b3524ccb30d453d4525023f311187
iholica <ir.bergolts@gmail.com>
2021-05-03 09:41:34 +0300

    update

---
+++ CarPark-main/CarPark/CarPark/FormCarPark.cs
@@ -0,0 +1,333 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CarPark
+{
+    public partial class FormCarPark : Form
+    {
+        DBEntities db;
+        ListLastChanges busChanges;
+        List<Bus> lastSaveList = new List<Bus>();
+        List<ListLastChanges> lastChanges = new List<ListLastChanges>();
+        ScriptEngine engine = Python.CreateEngine();
+        public FormCarPark()
+        {
+            InitializeComponent();
+
+            db = new DBEntities();
+            db.Bus.Load();
+            dataGridView1.DataSource = db.Bus.Local.ToBindingList();
+            ScriptScope scope = engine.CreateScope();
+            scope.SetVariable("count", dataGridView1.RowCount);
+            TotalCount.Text = scope.GetVariable("count").ToString();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+        //водители
+        private void водителиToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            FormDrivers form = new FormDrivers();
+            form.Show();
+        }
+        //типы автобусов
+        private void типыАвтобусовToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            FormTypesOfBuses form = new FormTypesOfBuses();
+            form.Show();
+        }
+        //состояние
+        private void состояниеАвтобусовToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            FormConditions form = new FormConditions();
+            form.Show();
+        }
+        //добавление автобуса
+        private void добавитьАвтобусToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            FormEditBus form_edit = new FormEditBus();
+
+            List<TypeBus> types = db.TypeBus.ToList();
+            form_edit.comboBox1.DataSource = types;
+            form_edit.comboBox1.ValueMember = "Id";
+            form_edit.comboBox1.DisplayMember = "Name";
+
+            List<Condition> conditions = db.Condition.ToList();
+            form_edit.comboBox2.DataSource = conditions;
+            form_edit.comboBox2.ValueMember = "Id";
+            form_edit.comboBox2.DisplayMember = "Name";
+
+            List<Driver> drivers = db.Driver.ToList();
+            form_edit.comboBox3.DataSource = drivers;
+            form_edit.comboBox3.ValueMember = "Id";
+            form_edit.comboBox3.DisplayMember = "LastName";
+
+            DialogResult result = form_edit.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            Bus bus = new Bus();
+            bus.Brand = form_edit.textBox1.Text;
+            bus.TypeBus = (TypeBus)form_edit.comboBox1.SelectedItem;
+            bus.Condition = (Condition)form_edit.comboBox2.SelectedItem;
+            bus.NumberOfSeats = Convert.ToInt32(form_edit.textBox2.Text);
+            bus.GovernmentNumber = form_edit.textBox3.Text;
+            bus.Driver = (Driver)form_edit.comboBox3.SelectedItem;
+            lastSaveList.Add(bus);
+            db.Bus.Add(bus);
+            db.SaveChanges();
+            
+            busChanges.id = lastSaveList.Last().Id;
+            busChanges.done = "add";
+            lastChanges.Add(busChanges);
+
+            ScriptScope scope = engine.CreateScope();
+            scope.SetVariable("count", db.Bus.Count());
+            TotalCount.Text = scope.GetVariable("count").ToString();
+
+            MessageBox.Show("Новый автобус добавлен.", "Автобусы", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        //редакирование записи автобуса
+        private void изменитьАвтобусToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                bool item = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out int id);
+                if (item == false)
+                    return;
+
+                Bus bus = db.Bus.Find(id);
+                Bus bus1 = (Bus)bus.Clone();
+                lastSaveList.Add(bus1);
+                busChanges.id = id;
+                busChanges.done = "changes";
+                lastChanges.Add(busChanges);
+                FormEditBus form_edit = new FormEditBus();
+
+                form_edit.textBox1.Text = bus.Brand;
+                form_edit.textBox2.Text = Convert.ToString(bus.NumberOfSeats);
+                form_edit.textBox3.Text = bus.GovernmentNumber;
+
+                List<TypeBus> types = db.TypeBus.ToList();
+                form_edit.comboBox1.DataSource = types;
+                form_edit.comboBox1.ValueMember = "Id";
+                form_edit.comboBox1.DisplayMember = "Name";
+                if (bus.TypeBus != null)
+                    form_edit.comboBox1.SelectedValue = bus.TypeBus.Id;
+
+                List<Condition> conditions = db.Condition.ToList();
+                form_edit.comboBox2.DataSource = conditions;
+                form_edit.comboBox2.ValueMember = "Id";
+                form_edit.comboBox2.DisplayMember = "Name";
+                if (bus.Condition != null)
+                    form_edit.comboBox2.SelectedValue = bus.Condition.Id;
+
+                List<Driver> drivers = db.Driver.ToList();
+                form_edit.comboBox3.DataSource = drivers;
+                form_edit.comboBox3.ValueMember = "Id";
+                form_edit.comboBox3.DisplayMember = "LastName";
+                if (bus.Driver != null)
+                    form_edit.comboBox3.SelectedValue = bus.Driver.Id;
+
+                DialogResult result = form_edit.ShowDialog();
+
+                if (result == DialogResult.Cancel)
+                    return;
+
+                bus.Brand = form_edit.textBox1.Text;
+                bus.TypeBus = (TypeBus)form_edit.comboBox1.SelectedItem;
+                bus.Condition = (Condition)form_edit.comboBox2.SelectedItem;
+                bus.NumberOfSeats = Convert.ToInt32(form_edit.textBox2.Text);
+                bus.GovernmentNumber = form_edit.textBox3.Text;
+                bus.Driver = (Driver)form_edit.comboBox3.SelectedItem;
+
+                db.Entry(bus).State = EntityState.Modified;
+                db.SaveChanges();
+
+                MessageBox.Show("Информация об автобусе изменена.", "Автобусы", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Выберите данные для редактирования!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        //удаление автобуса
+        private void удалитьАвтобусToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                bool item = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out int id);
+                if (item == false)
+                    return;
+                if (MessageBox.Show("Вы действительно желаете удалить выбранный автобус?", "Операция удаления автобуса", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Bus bus = db.Bus.Find(id);
+                    lastSaveList.Add(bus);
+                    busChanges.id = lastSaveList.Last().Id;
+                    busChanges.done = "delete";
+                    busChanges.ConditionId = lastSaveList.Last().IdCondition;
+                    busChanges.TypeId = lastSaveList.Last().IdTypeBus;
+                    busChanges.DriverId = lastSaveList.Last().IdDriver;
+                    lastChanges.Add(busChanges);
+                    db.Bus.Remove(bus);
+                    db.SaveChanges();
+                    ScriptScope scope = engine.CreateScope();
+                    scope.SetVariable("count", db.Bus.Count());
+                    TotalCount.Text = scope.GetVariable("count").ToString();
+                    MessageBox.Show("Информация о автобусе удалена.", "Автобусы", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+                MessageBox.Show("Выберите данные для удаления!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        //поиск по таблице
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            var search = db.Bus.Where(bus => bus.Brand.Contains(item)
+            | bus.Driver.LastName.Contains(item)
+            | bus.GovernmentNumber.Contains(item)
+            | bus.TypeBus.Name.Contains(item));
+
+            dataGridView1.DataSource = search.ToList();
+            ScriptScope scope = engine.CreateScope();
+            scope.SetVariable("count", search.ToList().Count());
+            TotalCount.Text = scope.GetVariable("count").ToString();
+        }
+
+        private void свободныеАвтобусыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var queryEmptyBus = db.Bus.Where(bus => bus.Condition.Name == "На базе");
+            dataGridView1.DataSource = queryEmptyBus.ToList();
+        }
+
+        private void автобусыВРейсеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var queryInFlightBus = db.Bus.Where(bus => bus.Condition.Name == "В рейсе");
+            dataGridView1.DataSource = queryInFlightBus.ToList();
+        }
+
+        private void автобусыВРемонтеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var queryUnderRepair = db.Bus.Where(bus => bus.Condition.Name == "В ремонте");
+            dataGridView1.DataSource = queryUnderRepair.ToList();
+        }
+
+        private void списанныеАвтобусыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var queryDecommissioned = db.Bus.Where(bus => bus.Condition.Name == "Списан");
+            dataGridView1.DataSource = queryDecommissioned.ToList();
+        }
+
+        private void всеАвтобусыАвтопаркаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var all = db.Bus.Select(allBus => allBus);
+            dataGridView1.DataSource = all.ToList();
+        }
+
+        private void информацияПоЗапросуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void информацияПоЗапросуToolStripMenuItem_DropDownClosed(object sender, EventArgs e)
+        {
+            menuStrip1.Items[1].ForeColor = Color.White;
+        }
+
+        private void информацияПоЗапросуToolStripMenuItem_DropDownOpened(object sender, EventArgs e)
+        {
+            menuStrip1.Items[1].ForeColor = Color.FromArgb(100, 160, 194);
+        }
+
+        private void справочникиToolStripMenuItem_DropDownClosed(object sender, EventArgs e)
+        {
+            menuStrip1.Items[0].ForeColor = Color.White;
+        }
+
+        private void справочникиToolStripMenuItem_DropDownOpened(object sender, EventArgs e)
+        {
+            menuStrip1.Items[0].ForeColor = Color.FromArgb(100, 160, 194);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(lastChanges.Count != 0)
+            {
+                switch (lastChanges.Last().done)
+                {
+                    case "add":
+                        {
+                            Bus bus = db.Bus.Find(lastChanges.Last().id);
+                            db.Bus.Remove(bus);
+                            db.SaveChanges();
+                            lastChanges.Remove(lastChanges.Last());
+                            lastSaveList.Remove(lastSaveList.Last());
+                            break;
+                        }
+                    case "delete":
+                        {
+                            Bus bus = new Bus();
+                            bus.Brand = lastSaveList.Last().Brand;
+
+                            bus.TypeBus = db.TypeBus.Find(lastChanges.Last().TypeId);
+                            bus.Condition = db.Condition.Find(lastChanges.Last().ConditionId);
+                            bus.NumberOfSeats = lastSaveList.Last().NumberOfSeats;
+                            bus.GovernmentNumber = lastSaveList.Last().GovernmentNumber;
+                            bus.Driver = db.Driver.Find(lastChanges.Last().DriverId);
+                            db.Bus.Add(bus);
+                            db.SaveChanges();
+                            lastChanges.Remove(lastChanges.Last());
+                            lastSaveList.Remove(lastSaveList.Last());
+                            break;
+                        }
+                    case "changes":
+                        {
+                            Bus bus = db.Bus.Find(lastChanges.Last().id);
+                            bus.Brand = lastSaveList.Last().Brand;
+                            bus.TypeBus = lastSaveList.Last().TypeBus;
+                            bus.Condition = lastSaveList.Last().Condition;
+                            bus.NumberOfSeats = lastSaveList.Last().NumberOfSeats;
+                            bus.GovernmentNumber = lastSaveList.Last().GovernmentNumber;
+                            bus.Driver = lastSaveList.Last().Driver;
+                            db.Entry(bus).State = EntityState.Modified;
+                            db.SaveChanges();
+                            lastChanges.Remove(lastChanges.Last());
+                            lastSaveList.Remove(lastSaveList.Last());
+                            break;
+                        }
+                    default:
+                        {
+                            MessageBox.Show("Нет последних изменений.", "Автобусы", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            break;
+                        }
+                }
+            }
+            else
+                MessageBox.Show("Нет последних изменений.", "Автобусы", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+
+    public struct ListLastChanges{
+
+        public int id;
+        public string done;
+        public int ConditionId;
+        public int TypeId;
+        public int? DriverId;
+    }
+}
---
+++ CarPark-main/CarPark/CarPark/FormDrivers.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarPark
+{
+    public partial class FormDrivers : Form
+    {
+        DBEntities db;
+        DriversStatus drStatus;
+        List<DriversStatus> DrList = new List<DriversStatus>();
+        List<Driver> DriverLastChanges = new List<Driver>();
+        public FormDrivers()
+        {
+            InitializeComponent();
+
+            db = new DBEntities();
+            db.Driver.Load();
+            dataGridView1.DataSource = db.Driver.Local.ToBindingList();
+        }
+        //добавление водителя
+        private void добавитьВодителяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormEditDriver form_edit = new FormEditDriver();
+
+            DialogResult result = form_edit.ShowDialog(this);
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            Driver driver = new Driver();
+            driver.LastName = form_edit.textBox1.Text;
+            driver.FirstName = form_edit.textBox2.Text;
+            driver.DateOfBith = form_edit.dateTimePicker1.Value;
+            driver.Address = form_edit.textBox3.Text;
+            driver.Phone = form_edit.textBox4.Text;
+            driver.Experience = form_edit.textBox5.Text;
+
+            DriverLastChanges.Add(driver);
+            
+            db.Driver.Add(driver);
+            db.SaveChanges();
+            drStatus.id = DriverLastChanges.Last().Id;
+            drStatus.status = "add";
+            DrList.Add(drStatus);
+
+            MessageBox.Show("Новый водитель добавлен.", "Водители", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        //редактировать водителя
+        private void изменитьВодителяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                bool item = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out int id);
+                if (item == false)
+                    return;
+
+                Driver driver = db.Driver.Find(id);
+                Driver driver1 = (Driver)driver.Clone();
+                DriverLastChanges.Add(driver1);
+                drStatus.id = DriverLastChanges.Last().Id;
+                drStatus.status = "change";
+                DrList.Add(drStatus);
+
+                FormEditDriver form_edit = new FormEditDriver();
+
+                form_edit.textBox1.Text = driver.LastName;
+                form_edit.textBox2.Text = driver.FirstName;
+                form_edit.dateTimePicker1.Value = driver.DateOfBith;
+                form_edit.textBox3.Text = driver.Address;
+                form_edit.textBox4.Text = driver.Phone;
+                form_edit.textBox5.Text = driver.Experience;
+
+                DialogResult result = form_edit.ShowDialog(this);
+
+                if (result == DialogResult.Cancel)
+                    return;
+
+                driver.LastName = form_edit.textBox1.Text;
+                driver.FirstName = form_edit.textBox2.Text;
+                driver.DateOfBith = form_edit.dateTimePicker1.Value;
+                driver.Address = form_edit.textBox3.Text;
+                driver.Phone = form_edit.textBox4.Text;
+                driver.Experience = form_edit.textBox5.Text;
+
+                db.Entry(driver).State = EntityState.Modified;
+                db.SaveChanges();
+
+                MessageBox.Show("Данные обновлены.", "Водители", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Выберите данные для редактирования!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        //удалить водителя
+        private void удалитьВодителяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+                bool item = Int32.TryParse(dataGridView1[0, index].Value.ToString(), out int id);
+                if (!item)
+                    return;
+                if (MessageBox.Show("Вы действительно желаете удалить выбранного водителя?", "Операция удаления водителя", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    Driver driver = db.Driver.Find(id);
+                    Driver driver1 = (Driver)driver.Clone();
+                    DriverLastChanges.Add(driver1);
+                    drStatus.id = DriverLastChanges.Last().Id;
+                    drStatus.status = "delete";
+                    DrList.Add(drStatus);
+                    db.Driver.Remove(driver);
+                    db.SaveChanges();
+                    MessageBox.Show("Данные удалены.", "Водители", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+                MessageBox.Show("Выберите данные для удаления!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DrList.Count != 0)
+            {
+                switch (DrList.Last().status)
+                {
+                    case "add":
+                        {
+                            Driver driver = db.Driver.Find(DriverLastChanges.Last().Id);
+                            db.Driver.Remove(driver);
+                            db.SaveChanges();
+                            DriverLastChanges.Remove(DriverLastChanges.Last());
+                            DrList.Remove(DrList.Last());
+                            break;
+                        }
+                    case "delete":
+                        {
+                            Driver driver = new Driver();
+                            driver.Address = DriverLastChanges.Last().Address;
+                            driver.DateOfBith = DriverLastChanges.Last().DateOfBith;
+                            driver.Experience = DriverLastChanges.Last().Experience;
+                            driver.FirstName = DriverLastChanges.Last().FirstName;
+                            driver.LastName = DriverLastChanges.Last().LastName;
+                            driver.Phone = DriverLastChanges.Last().Phone;
+                            driver.Bus = DriverLastChanges.Last().Bus;
+                            db.Driver.Add(driver);
+                            db.SaveChanges();
+                            DriverLastChanges.Remove(DriverLastChanges.Last());
+                            DrList.Remove(DrList.Last());
+                            break;
+                        }
+                    case "changes":
+                        {
+                            Driver driver = db.Driver.Find(DriverLastChanges.Last().Id);
+                            driver.Address = DriverLastChanges.Last().Address;
+                            driver.DateOfBith = DriverLastChanges.Last().DateOfBith;
+                            driver.Experience = DriverLastChanges.Last().Experience;
+                            driver.FirstName = DriverLastChanges.Last().FirstName;
+                            driver.LastName = DriverLastChanges.Last().LastName;
+                            driver.Phone = DriverLastChanges.Last().Phone;
+                            driver.Bus = DriverLastChanges.Last().Bus;
+                            db.Entry(driver).State = EntityState.Modified;
+                            db.SaveChanges();
+                            DriverLastChanges.Remove(DriverLastChanges.Last());
+                            DrList.Remove(DrList.Last());
+                            break;
+                        }
+                    default:
+                        {
+                            MessageBox.Show("Нет последних изменений.", "Автобусы", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            break;
+                        }
+                }
+            }
+            else
+                MessageBox.Show("Нет последних изменений.", "Автобусы", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+    public struct DriversStatus
+    {
+        public int id;
+        public string status;
+    }
+}
